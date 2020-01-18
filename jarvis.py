795f3a65c8a3457e955d976e50cb8fc99f0b4534
mohinesh9999 <mohineshsharma9999@gmail.com>
2020-01-18 22:01:57 +0530

    s

--- jarvis.py
+++ jarvis.py
@@ -7,6 +7,28 @@ import os
 from selenium import webdriver
 import smtplib
 import time 
+from pycricbuzz import Cricbuzz
+def cricCommentry():
+        c = Cricbuzz()
+        matches = c.matches()
+        # for match in matches:
+        match=matches[0]
+        # print( match)
+        # if(match['mchstate'] != 'nextlive'):
+        #         print (c.livescore(match['id']))
+        #         print( c.commentary(match['id']))
+        #         print( c.scorecard(match['id']))
+        print('sun',c.commentary(match['id'])['commentary'][0]['comm'])
+        a,b=c.commentary(match['id'])['commentary'][0]['comm'],''
+        while(1):
+                if(a!=b):
+                        # speak(a)
+                        d=c.scorecard(match['id'])['scorecard']
+                        speak(a)
+                        speak(d[0]['batteam']+'score is '+d[0]['runs']+'for'+d[0]['wickets']+'wickets')
+                        b=a
+                else:
+                        a=c.commentary(match['id'])['commentary'][0]['comm']
 
 browser = webdriver.Chrome(executable_path=r'C:\web-driver\chromedriver.exe') 
 
@@ -127,3 +149,9 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
                 speak("Sorry my friend harry bhai. I am not able to send this email")    
+        elif 'commentary' in query.lower():
+             try:
+                cricCommentry()
+             except Exception as e:
+                print(e)
+                speak("Sorry my friend harry bhai. I am not able to send this email")   
--- testAPI.py
+++ testAPI.py
@@ -1,10 +1,23 @@
 from pycricbuzz import Cricbuzz
-c = Cricbuzz()
-matches = c.matches()
-# for match in matches:
-match=matches[0]
-print( match)
-if(match['mchstate'] != 'nextlive'):
-        print (c.livescore(match['id']))
-        print( c.commentary(match['id']))
-        print( c.scorecard(match['id']))
+def cricCommentry():
+        c = Cricbuzz()
+        matches = c.matches()
+        # for match in matches:
+        match=matches[0]
+        # print( match)
+        if(match['mchstate'] != 'nextlive'):
+                print (c.livescore(match['id']))
+                print( c.commentary(match['id']))
+                print( c.scorecard(match['id']))
+        print('sun',c.commentary(match['id'])['commentary'][0]['comm'])
+        a,b=c.commentary(match['id'])['commentary'][0]['comm'],''
+        while(1):
+                if(a!=b):
+                        # speak(a)
+                        d=c.scorecard(match['id'])['scorecard']
+                        print(a)
+                        print(d[0]['batteam'],'score is ',d[0]['runs'],'for',d[0]['wickets'],'wickets')
+                        b=a
+                else:
+                        a=c.commentary(match['id'])['commentary'][0]['comm']
+cricCommentry()
